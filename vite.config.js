7c008a67dea5f68f149ab7f40bcf5f2022d27a4f
Rafael Tury Minatel <rafaelturyminatel@gmail.com>
2023-04-29 00:05:22 -0300

    Fix

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: '../public',
-    sourcemap: true,
-    rollupOptions: {
-      input: {
-        main: './index.html',
-      }
-    }
-  }
+
 })
