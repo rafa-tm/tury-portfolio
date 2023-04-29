917b53ec6472934ce013a7a0beb172b8426c64ea
Rafael Tury Minatel <rafaelturyminatel@gmail.com>
2023-04-28 23:24:43 -0300

    Test

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: '../public',
+    assetsDir: './dist',
+    sourcemap: true,
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        nested: './nested/index.html'
+      }
+    }
+  }
 })
