9a9f193b7ef5fb2ad5ccede0b01581fa5da69a87
Rafael Tury <rafaelturyminatel@gmail.com>
2023-05-15 11:29:41 -0300

    remove VLibras

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { ThemeProvider } from "./providers/theme.jsx";
-import VLibras from "@djpfs/react-vlibras";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider>
-      <VLibras />
       <App />
     </ThemeProvider>
   </React.StrictMode>
